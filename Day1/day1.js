95b5275a9709c5833337925c95c3c232964ef73f
YogeshWEB <neemaker246@gmail.com>
2024-07-13 18:30:35 +0530

    Day1

---
+++ Day1/day1.js
@@ -0,0 +1,69 @@
+//task1 Activity -1
+var task1 = 10;
+console.log(task1);
+let task2= "Hello string";
+console.log(task2);
+const task3= true;
+console.log(task3);
+
+// Task 4 **********************************************************************
+let num=1;
+let str="string";
+let booly = true;
+let obj= {
+    name:"MyName",
+    age:22
+};
+let arr = [1,2,3];
+
+ console.log(typeof(num));
+ console.log(typeof(str));
+ console.log(typeof(booly));
+ console.log(typeof(obj));
+ console.log(typeof arr);
+ 
+//  Task5 Reassigning variables
+let ree1= "first value let";
+console.log(ree1);
+ree1= "new first value let";
+console.log(ree1);
+
+//  Task6 Reassigning variables
+const ree2= "first value const";
+console.log(ree2);
+ree2= "new first value const";//error
+console.log(ree2);
+
+
+
+//! ----------------------------- Feature Request ----------------------------- //
+
+let number =10;
+let string = "string";
+let bool = true;
+let no = null;
+let undef= undefined;
+let obj1 = {hello:"chai"};
+let arr1= [1,2,3];
+
+console.log(`${number} is ${typeof number} \n
+            ${string} is ${typeof string} \n 
+            ${bool} is ${typeof bool} \n
+            ${no} is ${typeof no} \n
+            ${undef} is ${typeof undef} \n
+            ${obj1} is ${typeof obj1} \n
+            ${arr1} is ${typeof arr1} \n
+             `)
+
+
+// reassignment demo
+
+            let re1= "first value let";
+            console.log(re1);
+            re1= "new first value let";
+            console.log(re1);
+
+             const re2= "first value const";
+             console.log(re2);
+             re2= "new first value const";//error
+             console.log(re2);
